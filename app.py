386fc07ff9ed5b6b11388adcd0700ce57a86f2a0
frankzheng <frank.zheng@liteon.com>
2025-05-15 17:54:51 +0800

    VII

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 import datetime
 from flask import Flask, request, abort, jsonify
 
-from linebot.v3.webhooks import WebhookParser
+from linebot.v3.webhook import WebhookParser
 from linebot.v3.messaging import MessagingApi, Configuration, ApiClient
 from linebot.v3.messaging.models import ReplyMessageRequest, TextMessage
 from linebot.v3.webhooks.models import MessageEvent, TextMessageContent
